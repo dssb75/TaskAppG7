88275f66b89afcb9f1e0fb25dd998d7ca969e736
MillerC <miller20719@gmail.coml>
2026-04-22 10:35:43 -0500

    Ajuste interfaz menu y fondo

--- app/src/main/java/com/example/grupo7/MainActivity.kt
+++ app/src/main/java/com/example/grupo7/MainActivity.kt
@@ -5,26 +5,41 @@ import androidx.fragment.app.Fragment
 
 
 class MainActivity : AppCompatActivity(), MenuFragment.OnOptionClickListener {
+    //DEFINICION DE VARIABLES
+    private val boton = "Boton"
+    private val web = "Web"
+    private val video = "Video"
+    private val fotos = "Photos"
+    private val perfil = "Profile"
 
+    //METODOS PRINCIPALES
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         if (savedInstanceState == null) {
-            onOptionClicked("profile")
+            onOptionClicked(perfil)
         }
     }
 
     override fun onOptionClicked(option: String) {
         val fragment: Fragment = when (option) {
-            "photos" -> PhotosFragment()
-            "video" -> VideoFragment()
-            "web" -> WebFragment()
-            "Boton" -> ButtonFragment()
-            else -> ProfileFragment() // Caso por defecto
+            fotos -> PhotosFragment()
+            video -> VideoFragment()
+            web -> WebFragment()
+            boton -> ButtonFragment()
+            else -> ProfileFragment()
         }
+        cambioColor(option)
         supportFragmentManager.beginTransaction()
             .replace(R.id.content_fragment_container, fragment)
             .addToBackStack(null)
             .commit()
+    }
+
+    //METODOS SECUNDARIOS
+    private fun cambioColor(option: String) {
+        val menuFragment = supportFragmentManager
+            .findFragmentById(R.id.menu_fragment_container) as? MenuFragment
+        menuFragment?.cambioColor(option)
     }
 }
